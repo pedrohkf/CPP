58170ced9a9b00869a87cc83b3ec9f72dc8c43f0
PEDRO HENRIQUE KAUFMANN FARIA <CJ3040895@ifspcjoad.ifspcjo.edu.br>
2026-07-29 22:27:01 -0300

    aula com o primeiro codigo

---
+++ aula01/main.cpp
@@ -0,0 +1,13 @@
+// # diretitva <biblioteca>
+#include <iostream>
+
+int main(void)
+{
+	
+	std::cout << "Olá, mundo!" << std::endl;
+	
+	std::cout << "Tecla <Enter> para encerrar...";
+	std::cin.get();
+	
+	return 0;
+}
