70e07b15677248d3e356001171c5bbd5285e3dec
PEDRO HENRIQUE KAUFMANN FARIA <CJ3040895@ifspcjoad.ifspcjo.edu.br>
2026-08-05 20:12:00 -0300

    adicionando importação forma do mercado

--- aula02/ex02.cpp
+++ aula02/ex02.cpp
@@ -2,15 +2,17 @@
 #include <iostream>
 
 //forma educativa de fazer
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main(void)
 {
 	
 	cout << "Ola, mundo!" << endl;
 	
-	//std::cout << "Tecla <Enter> para encerrar...";
-	//std::cin.get();
+	std::cout << "Tecla <Enter> para encerrar...";
+	std::cin.get();
 	
 	return 0;
 }
