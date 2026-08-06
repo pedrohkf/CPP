0cc244bf458b63cf0f0e9963d6a88041656b52f8
PEDRO HENRIQUE KAUFMANN FARIA <CJ3040895@ifspcjoad.ifspcjo.edu.br>
2026-08-05 22:01:27 -0300

    adicionando programa que aceita portugues brasileiro

---
+++ aula02/C02EX5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <locale> // para setLocale()
+#include <windows.h> // para SetConsoleCP()
+#include <string> // para usar std::wstring
+
+using namespace std;
+
+int main(void)
+{
+	wstring nome;
+	
+	setlocale(LC_ALL, "Portuguese_Brazil.1252"); 
+	SetConsoleCP(1252);
+	
+	wcout << L"Ola, \nQual sua graça? ";
+	getline(wcin, nome);
+	
+	cout << endl;
+	wcout << "Oi " << nome << ", vamos estudar. " << endl;
+	cout << endl;
+	
+	cout << "Tecla <Enter> para encerrar...";
+	cin.get();
+	return 0;
+}
+
