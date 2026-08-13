1ac4559cfc793cc3b802e16ddb51222749c5f951
PEDRO HENRIQUE KAUFMANN FARIA <CJ3040895@ifspcjoad.ifspcjo.edu.br>
2026-08-12 22:35:21 -0300

    adicionando aulas prontas

---
+++ aula03/C02EX11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(void)
+{
+	bool x;
+	
+	x = true;
+	cout << "Valor de X como verdadeiro ...: " << x << endl;
+	
+	x = false;
+	cout << "Valor de X como falso ...: " << x << endl;
+	
+	cout << endl;
+	cout << "Tecla <Enter> para encerrar...";
+	cin.get();
+	return 0;
+}
