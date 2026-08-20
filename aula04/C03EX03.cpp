e1f287771823b78b75ea108484e87f4e895499a4
PEDRO HENRIQUE KAUFMANN FARIA <CJ3040895@ifspcjoad.ifspcjo.edu.br>
2026-08-19 22:21:24 -0300

    adicionando aula 04

---
+++ aula04/C03EX03.cpp
@@ -0,0 +1,32 @@
+// C03EX01.CPP
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  int32_t a, b, x;
+  
+  cout << "Entre o primeiro valor ...: "; cin >> a;
+  cin.ignore(80, '\n');
+  
+  cout << "Entre o segundo valor ....: "; cin >> b;
+  cin.ignore(80, '\n');
+
+  cout << "\n";
+  
+  if (a > b)
+  {
+	x = a;
+	a = b;
+	b = x;
+  }
+  
+  cout << "Os valores ordenados sao: " << a;
+  cout << " e " << b << "." << endl << endl;
+    
+  cout << "Tecle <Enter> para encerrar... ";
+  cin.get();
+  return 0;
+}
